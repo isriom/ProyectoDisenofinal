227c1458d7d34627712f68f0eec1d48927c7e53c
Justin Fernández <67604304+JustinFB18@users.noreply.github.com>
2023-05-29 17:22:09 -0600

    Cambios BL y R5 en lugar de R4

--- Proyecto.s
+++ Proyecto.s
@@ -1,14 +1,14 @@
-; Definición de registros. Estos son mandados en el modulo de red
-.equ GPIO_BASE, 0x20000000      ; Dirección base del registro GPIO
+; DefiniciÃ³n de registros. Estos son mandados en el modulo de red
+.equ GPIO_BASE, 0x20000000      ; DirecciÃ³n base del registro GPIO
 .equ GPIO_SET_OFFSET, 0x04      ; Final del registro GPIO 
 
-.equ SENSOR_BASE, 0x00      ; Dirección base del registro de Sensores
+.equ SENSOR_BASE, 0x00      ; DirecciÃ³n base del registro de Sensores
 .equ SENSOR_SET_OFFSET, 0x82      ; Final del registro Sensores 
 
 .data
 piso:    .word 3      ; Variable para almacenar el piso disponible
-num_piso:     .word 43      ; Variable para almacenar el número de estacionamiento disponible
-num_total:     .word 129      ; Variable para almacenar el número de estacionamiento disponible
+num_piso:     .word 43      ; Variable para almacenar el nÃºmero de estacionamiento disponible
+num_total:     .word 129      ; Variable para almacenar el nÃºmero de estacionamiento disponible
 
 .text
 .global _start
@@ -19,29 +19,29 @@ _start:
 	ldr r1, =SENSOR_BASE
 	mov r2, #0 ; flag de enviar datos por medio de GPIO_BASE y salir del loop;
 	;r3 para calculos
-	mov r4, #129       ; Número total de estacionamientos
+	mov r4, #129       ; NÃºmero total de estacionamientos
     mov r5, #0         ; Contador de iteraciones
-    mov r6, #0         ; Número de estacionamiento
+    mov r6, #0         ; NÃºmero de estacionamiento
     mov r7, #0         ; Piso
 
 loop:;---Falta Agregar salida del loop---
     ; Leer el estado de los sensores
 	add r1, r1, #1; Desplazar al siguiente sensor
     ldr r3, r1; Cargar valor de sensor
-	b check_sensors
-	add r4, r4, #1;
-	b loop
+	BL check_sensors
+	add r5, r5, #1;
+	B loop
 
 check_sensors:
     tst r3, #1         ; Verificar el bit menos significativo (Sensor de estacionamiento actual)
     beq sensor_no_ocupado       ; Saltar si el bit es 0 (espacio no ocupado)
-	b loop ;Si no volver al loop
+	BX LR ;Si no volver al loop
 
 sensor_no_ocupado:
-    ; Calcular el piso y el número de estacionamiento
-    ldr r3, =43       ; Número de estacionamientos por piso, udiv no puede usar inmediato
-    udiv r7, r5, r3   ; Cálculo del piso (usando división entera)
-    add r6, r5, r7      ; Número de estacionamiento (comienza desde 0)
+    ; Calcular el piso y el nÃºmero de estacionamiento
+    ldr r3, =43       ; NÃºmero de estacionamientos por piso, udiv no puede usar inmediato
+    udiv r7, r5, r3   ; CÃ¡lculo del piso (usando divisiÃ³n entera)
+    add r6, r5, r7      ; NÃºmero de estacionamiento (comienza desde 0)
 
 	mov r2,#1; Mandar datos y flag para detener loops
 	b Send_byte_UART;
