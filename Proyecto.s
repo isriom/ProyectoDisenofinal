0f940852b7f4b1897dc06eecdc68711032fd0b33
Justin Fernández <67604304+JustinFB18@users.noreply.github.com>
2023-05-30 00:13:50 -0600

    Cargado a GPIO

--- Proyecto.s
+++ Proyecto.s
@@ -1,17 +1,31 @@
-; Definición de registros. Estos son mandados en el modulo de red
-.equ GPIO_BASE, 0x20000000      ; Dirección base del registro GPIO
-.equ GPIO_SET_OFFSET, 0x04      ; Final del registro GPIO 
+	AREA RESET, DATA, READONLY
+		EXPORT __Vectors
 
-.equ SENSOR_BASE, 0x00      ; Dirección base del registro de Sensores
-.equ SENSOR_SET_OFFSET, 0x82      ; Final del registro Sensores 
+__Vectors
+	DCD 0x20001000 ; stack pointer value when stack is empty
+	DCD Reset_Handler ; reset vector
+	ALIGN
+		
+	AREA FSM, CODE, READONLY
+	
+	ENTRY
+	EXPORT Reset_Handler	
+		
+Reset_Handler
 
-.data
-piso:    .word 3      ; Variable para almacenar el piso disponible
-num_piso:     .word 43      ; Variable para almacenar el número de estacionamiento disponible
-num_total:     .word 129      ; Variable para almacenar el número de estacionamiento disponible
+	; Definición de registros. Estos son mandados en el modulo de red
+	.equ GPIO_BASE, 0x20000000      ; Dirección base del registro GPIO
+	.equ GPIO_SET_OFFSET, 0x04      ; Final del registro GPIO 
 
-.text
-.global _start
+	.equ SENSOR_BASE, 0x00      ; Dirección base del registro de Sensores
+	.equ SENSOR_SET_OFFSET, 0x82      ; Final del registro Sensores 
+
+	.data
+	piso:    .word 3      ; Variable para almacenar el piso disponible
+	num_piso:     .word 43      ; Variable para almacenar el número de estacionamiento disponible
+	num_total:     .word 129      ; Variable para almacenar el número de estacionamiento disponible
+
+	.text
 
 _start
     ; Configurar los pines de los sensores como entradas y el GPIO como salida
@@ -43,10 +57,13 @@ sensor_no_ocupado
     	; Calcular el piso y el número de estacionamiento
     	ldr r3, =43       	; Número de estacionamientos por piso, udiv no puede usar inmediato
     	udiv r7, r5, r3   	; Cálculo del piso (usando división entera)
-    	add r6, r5, r7      ; Número de estacionamiento (comienza desde 0)
-
+    	sub r6, r5, r7      ; Número de estacionamiento (comienza desde 0)
+	mult r3, r6, r3 ; 
+	
 	mov r2,#1; Mandar datos y flag para detener loops
 	b Send_byte_UART;
 
 Send_byte_UART:	;---cargar a GPIO los datos(Piso y numero)---
+	STR r7, [r0, #1] ; carga el piso
+	STR r6, [r0, #1] ; carga el número de estacionamiento
 	b loop
